360f4f06a0e19ba75d80c9feb89e5f272eff8379
Alvin <alvin.dev@outlook.com>
2016-02-09 02:59:35 +1000

    Added a static profile method

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ class Timer {
     var t = this.getElapsed()
     return '' + t[0] + '.' + (t[1] + 1e9).toString().slice(1);
   }
+
+  static profile(func, n) {
+    let timer = new Timer();
+    for (let i = 0; i < n; i++)
+      func();
+    return timer.toString();
+  }
 }
 
 module.exports = Timer;
